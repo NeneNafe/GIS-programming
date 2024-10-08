b648049ff2b71590dfe13d33ec2bca287416ae3a
NeneNafe <nenechiwaya@gmail.com>
2024-10-08 21:53:26 +0200

    Exercise 1: Variable Assignment and Basic Operations

---
+++ Lab1/Exercise/exercise1.py
@@ -0,0 +1,14 @@
+latitude = 40.7128
+longitude = -74.0060
+population = 8336817
+area_km2 = 783.8
+
+New_York_City = (population / area_km2)
+
+print("The population density of New York City per square kilometer is ", 
+    New_York_City,"km2")
+print("New York City lies at a latitude of", latitude, "and a longitude of", longitude)
+
+lat = list(latitude)
+lon = list(longitude)
+print("Latitude:", lat, "Longitude:", lon)

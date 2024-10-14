d290e664d81e8bd429c49e7e46edb6acf5a8466c
NeneNafe <nenechiwaya@gmail.com>
2024-10-14 14:15:23 +0200

    Corrections on the the “Latitude: [latitude], Longitude: [longitude]”

--- Lab1/Exercise/exercise1.py
+++ Lab1/Exercise/exercise1.py
@@ -1,3 +1,7 @@
+# Creates variables that store geo-cordinates
+# Calculate and print the population density of New York City
+# Print the coordinates in the format “Latitude: [latitude], Longitude: [longitude]”
+
 latitude = 40.7128
 longitude = -74.0060
 population = 8336817
@@ -9,6 +13,4 @@ print("The population density of New York City per square kilometer is ",
     New_York_City,"km2")
 print("New York City lies at a latitude of", latitude, "and a longitude of", longitude)
 
-lat = list(latitude)
-lon = list(longitude)
-print("Latitude:", lat, "Longitude:", lon)
+print(f"Latitude: {[latitude]}, Longitude: {[longitude]}")

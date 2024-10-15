e0b1f99530f67984e5a0448b8fd8338cce4ef45c
NeneNafe <nenechiwaya@gmail.com>
2024-10-15 21:35:11 +0200

    corrections

--- Lab1/Exercise/exercise10.py
+++ Lab1/Exercise/exercise10.py
@@ -10,14 +10,12 @@ locations = [
 # Create a new list that contains only the names of the locations
 def names_of_locations(locations):
     return [location[0] for location in locations]
-
 print(names_of_locations(locations))
 
 # Create a dictionary where the keys are location names and
 # the values are tuples of their coordinates
 def local_dictionary(locations):
     return {location[0]: (location[1], location[2]) for location in locations}
-
 print(local_dictionary(locations))
 
 # Print the latitude for "K2" using the dictionary

a35fe8871aceabd04021c3a648c3b1cb9f87a5ad
NeneNafe <nenechiwaya@gmail.com>
2024-10-29 11:45:41 +0200

    Using For Loops to Process Coordinate Lists

---
+++ Lab2/Exercises/exercise6.py
@@ -0,0 +1,17 @@
+# A list of tuples containing coordinates
+# Determin whether the coordinate is from the North pr South Pore
+
+list_of_coordinates = [
+    (40.7128, 74.0060),
+    (48.8566, 2.3522),
+    (-35.6895, 139.6917),
+    (30.0444, 31.2357),
+    (-33.9249, 18.4241)
+]
+
+for lat, lon in list_of_coordinates:
+    if lat > 0:
+        hemisphere = "Northern"
+    else:
+        hemisphere = "Southern"
+    print(f"The Coordinate ({lat}, {lon}) is in the {hemisphere} Hemisphere")

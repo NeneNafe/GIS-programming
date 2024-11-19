dcd173d24db011b07e46cc3a35cbd36e9eac1827
NeneNafe <nenechiwaya@gmail.com>
2024-11-19 10:33:24 +0200

    Creating and Manipulating GeoDataFrames with GeoPandas

---
+++ Lab4/exercise3.py
@@ -0,0 +1,9 @@
+# Create a plot of the building footprints and color them based on the building height
+# An interactive map of the building footprints and color them
+# based on the building height
+# Calculate the average building height
+# Select buildings with a height greater than the average height
+# Save the GeoDataFrame to a new GeoJSON file
+
+import pandas as pd
+ 
